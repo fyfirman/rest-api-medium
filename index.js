170c8715abb5f2edcd216441c4a64643a1b3c777
Firmansyah Yanuar <fyfirman@gmail.com>
2020-10-27 17:08:32 +0700

    add comment hello world

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const app = express();
 
+/*
+Handle alamat / , lalu kirimkan response hello world
+*/
 app.get('/', (req, res) => {
   res.json({ message: "Hello world" });
 })
 
+/*
+Sediakan server pada localhost dengan port 3000
+*/
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
